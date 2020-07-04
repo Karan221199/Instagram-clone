a48defc9388aecd4dd17132ee226a278ce4ed83c
Karan Middha <karanmiddha2299@gmail.com>
2020-07-04 21:03:17 +0530

    context api and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useEffect,createContext,useReducer,useContext} from 'react';
 import Navbar from './components/Navbar'
 import "./App.css"
 import Home from './components/screens/Home'
@@ -6,31 +6,58 @@ import Profile from './components/screens/Profile'
 import SignUp from './components/screens/SignUp'
 import Signin from './components/screens/Signin'
 import CreatePost from './components/screens/CreatePost'
+import {BrowserRouter,Route,Switch,useHistory} from 'react-router-dom'
+import {reducer,initialState} from './reducers/userReducer'
+export const UserContext= createContext()
 
-import {BrowserRouter,Route} from 'react-router-dom'
+const Routing=()=>{
+  const history=useHistory()
+  const {state,dispatch} =useContext(UserContext)
+  useEffect(()=>{
+    const user=JSON.parse(localStorage.getItem("user"))
+    if(user){
+      dispatch({type:"USER",payload:user})
+      history.push('/')
+    }
+    else
+    {
+      history.push('/signin')
+    }
+  },[])
+  return(
+    <Switch>
+    <Route exact path="/">
+    <Home />
+  </Route>
+
+  <Route path="/signin">
+    <Signin />
+  </Route>
+
+  <Route path="/Signup">
+    <SignUp />
+  </Route>
+
+  <Route path="/profile">
+    <Profile />
+  </Route>
+  <Route path="/create">
+    <CreatePost />
+  </Route>
+  </Switch>
+
+  )
+}
+
 function App() {
+  const [state,dispatch]=useReducer(reducer,initialState)
   return (
+    <UserContext.Provider value={{state,dispatch}}>
     <BrowserRouter>
    <Navbar />
-   <Route exact path="/">
-     <Home />
-   </Route>
-
-   <Route path="/signin">
-     <Signin />
-   </Route>
-
-   <Route path="/Signup">
-     <SignUp />
-   </Route>
-
-   <Route path="/profile">
-     <Profile />
-   </Route>
-   <Route path="/create">
-     <CreatePost />
-   </Route>
+  <Routing />
    </BrowserRouter>
+   </UserContext.Provider>
     
   );
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,31 @@
 // sun jb b hm anchor tag use krte h tb na page refresh hota h to uski jgh hm link use krte h
 
-import React from 'react'
+import React, {useContext} from 'react'
 import {Link} from 'react-router-dom'
+import {UserContext} from '../App'
 const Navbar = ()=>{
+  const {state,dispatch} = useContext(UserContext)
+  const renderList = ()=>{
+    if(state){
+        return [
+          <li><Link to="/profile">Profile</Link></li>,
+          <li><Link to="/create">Create</Link></li>
+        ]
+    }
+    else{
+        return [
+          <li><Link to="/signin">Signin</Link></li>,
+          <li><Link to="/signup">SignUp</Link></li>
+        ]
+    }
+  }
+
    return( 
   <nav>
   <div className="nav-wrapper white" >
-    <Link to="/" className="brand-logo left">Instagram</Link>
+    <Link to={state? "/" : "/signin"} className="brand-logo left">Instagram</Link>
     <ul id="nav-mobile" className="right hide-on-med-and-down">
-      <li><Link to="/signin">Signin</Link></li>
-      <li><Link to="/signup">SignUp</Link></li>
-      <li><Link to="/profile">Profile</Link></li>
-      <li><Link to="/create">Create</Link></li>
+      {renderList()}
     </ul>
   </div>
 </nav>
